f94f73c0f15a0170736761d719a74d0912451113
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-20 22:51:11 +0700

    feat: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PredictModule } from './predict/predict.module';
+import { CommonModule } from './common/common.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'node:path';
 
 @Module({
-  imports: [PredictModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PredictModule,
+    CommonModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.join(process.cwd(), 'public'), // Akses folder 'public' di root
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
