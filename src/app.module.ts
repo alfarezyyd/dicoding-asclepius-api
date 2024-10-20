5058f5dfa7bd17f89a619eba289cf6998742c821
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-20 22:13:29 +0700

    feat: add some package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PredictModule } from './predict/predict.module';
 
 @Module({
-  imports: [],
+  imports: [PredictModule],
   controllers: [AppController],
   providers: [AppService],
 })
