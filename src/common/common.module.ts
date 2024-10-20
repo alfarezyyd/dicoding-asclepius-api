b4dd65bf70c7c98cd76f02679a07ac4c154673ef
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-21 00:57:19 +0700

    feat: load model module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,15 +1,17 @@
 import { MulterService } from './multer.service';
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
-import { ModelHelper } from '../helper/model.helper';
+import { ConfigModule } from '@nestjs/config';
+import { ModelService } from './model.service';
 
 @Module({
   imports: [
     MulterModule.registerAsync({
       useClass: MulterService,
     }),
+    ConfigModule.forRoot({}),
   ],
-  providers: [MulterService, ModelHelper],
-  exports: [MulterModule, ModelHelper],
+  providers: [MulterService, ModelService],
+  exports: [MulterModule, ModelService],
 })
 export class CommonModule {}
