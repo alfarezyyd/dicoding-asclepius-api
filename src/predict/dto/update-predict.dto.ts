8cf578f8cadeb8973c4ed5e0f3dda27ab8b81a95
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-20 22:40:55 +0700

    feat: delete some file

--- src/predict/dto/update-predict.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePredictDto } from './create-predict.dto';
-
-export class UpdatePredictDto extends PartialType(CreatePredictDto) {}
